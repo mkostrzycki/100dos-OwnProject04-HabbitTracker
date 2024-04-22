73ca3990c01f4cbe10974855af8228cbfb1ef4b0
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-22 16:50:32 +0200

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by MaćKo on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
